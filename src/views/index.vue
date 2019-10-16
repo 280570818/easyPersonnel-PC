a0071a70be3be079fce0d40c7a4d01e18c54593e
swing <accp123>
2019-10-16 09:22:19 +0800

    init

---
+++ src/views/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>1111111</h1>
+    <h2>{{msg}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
